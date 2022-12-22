82622fc8b088fb0fc08eda163991d1f41a58c2cd
Rudraprasad-Mohapatra <rudraprasadmohapatra51@gmail.com>
2022-12-22 11:35:06 +0530

    2nd commit and only change the file string2.java

--- problems_on_string/string2.java
+++ problems_on_string/string2.java
@@ -1,6 +1,8 @@
-// Count number of vowels, consonants, spaces in String
+/c stat/ Count number of vowels, consonants, spaces in String
 package problems_on_string;
 
 public class string2 {
-    
-}
+    public static void main(String[] args) {
+    System.out.println("I am String2.java");
+    }
+  }
