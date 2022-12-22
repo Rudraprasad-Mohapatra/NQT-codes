da16b8c1e91371f51755e8cf1f2296e97165560b
Rudraprasad-Mohapatra <rudraprasadmohapatra51@gmail.com>
2022-12-22 11:46:07 +0530

    Changing string_three.java on branch clone_NQT_master

--- problems_on_string/string_three.java
+++ problems_on_string/string_three.java
@@ -10,6 +10,5 @@ public class string_three {
         Scanner sc=new Scanner(System.in);
          char num =sc.next().charAt(0);
           System.out.println((int)num);
-        
-    }
-}
+        } 
+      }
